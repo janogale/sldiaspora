ecefeb3c0146cacc6bb924909ae3a6af7943ff71
moshibbin <moshibbin@gmail.com>
2025-09-13 21:06:48 +0300

    added blogs

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ import Hero from "./components/hero";
 import ChooseUs from "./components/choose-us";
 import LatestProject from "./components/latest-project";
 import ProcessSection from "./components/process-section";
+import VisaCategory2 from "./components/visa-category-2";
+import VisaCategory from "./components/visa-category";
+import Blogs from "./components/blogs";
+import Testimonial from "./components/testimonial";
+import TicketBooking from "./components/ticket-booking";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
@@ -13,7 +19,14 @@ export default function Home() {
         <ChooseUs />
         <ProcessSection />
         <LatestProject />
+        <VisaCategory2 />
+        <div style={{ backgroundColor: "#006D21", height: "70px" }}></div>
+        <VisaCategory />
+        <Testimonial />
+        <TicketBooking />
+        <Blogs />
       </main>
+      <Footer />
     </>
   );
 }
