4ea7c7c8fded60ef1b7f8aefabfd6fee0adf4fdc
moshibbin <moshibbin@gmail.com>
2025-09-15 20:50:30 +0300

    fixed error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Footer from "./components/footer";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,8 +30,14 @@ export default function RootLayout({
       <body>
         {children}
         <Footer />
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <Script
+          src="https://code.jquery.com/jquery-3.6.0.min.js"
+          strategy="beforeInteractive" // loads before page is interactive
+        />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+          strategy="afterInteractive" // loads after hydration
+        />
       </body>
     </html>
   );
