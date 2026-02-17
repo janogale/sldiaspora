130e16dd66caca3c0aaea5033d9cd764b5cfb162
eng-shaab <shaab1012@gmail.com>
2026-02-17 17:11:21 +0300

    update

--- app/constants.ts
+++
@@ -1,6 +0,0 @@
-export const GLOBE_IMAGE_URL =
-  "//unpkg.com/three-globe/example/img/earth-night.jpg";
-export const BUMP_IMAGE_URL =
-  "//unpkg.com/three-globe/example/img/earth-topology.png";
-export const BACKGROUND_URL =
-  "//unpkg.com/three-globe/example/img/night-sky.png";
--- lib/directus.ts
+++
@@ -1,8 +0,0 @@
-import { createDirectus, rest } from '@directus/sdk';
-import { Schema } from './schema';
-
-
-const directus = createDirectus<Schema>('https://admin.sldiaspora.org').with(rest());
-    
-
-export default directus;
